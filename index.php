f8c6973858f278824ea3f6e51628dd37a3937043
vince7ben23 <vince7ben23@gmail.com>
2017-02-23 00:27:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include_once("utf8_header.php");
 <head>
 	<title>Bitcheese飾品</title>
 	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">    
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!--0219-->
@@ -73,7 +73,7 @@ include_once("utf8_header.php");
 	<div></div>
 	
 	<div id="left-shopping-cart"><!--左側鑲版-->
-		<div id="shopping-cart-title" class="special-font">Bitcheese</div>
+		<div id="shopping-cart-title" class="special-font">Bitcheese</div>nnnnn
 			<hr class="horizon">
 				
 			<div id="for_scroll">
