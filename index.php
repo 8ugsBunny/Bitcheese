e12a1ecd96b384903cbcf0cec1e71d5ea4c4061b
vince7ben23 <vince7ben23@gmail.com>
2017-02-22 23:40:16 +0800

    Ben_Test

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ include_once("utf8_header.php");
 	
 
 	
-	<div id="left-panel"><!--左側鑲版-->
+	<div id="left-panel" class="fuck mom"><!--左側鑲版--><!--test-->
 		<div id="category-title" class="special-font">Bitcheese</div>
 			<hr class="horizon">
 				<ul>
@@ -65,9 +65,10 @@ include_once("utf8_header.php");
 		</div>
 	
 	</div>
-
-	
-	
+	<div></div>
+<div></div>
+	<div></div><div></div>
+	<div></div>
 	
 	<div id="left-shopping-cart"><!--左側鑲版-->
 		<div id="shopping-cart-title" class="special-font">Bitcheese</div>
